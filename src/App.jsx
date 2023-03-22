f0101775ba125a66b83718bac9e4ab8318a6e920
unknown <almira.sagynbek@gmail.com>
2023-03-22 14:38:43 +0600

    Creating a UI library. First components. CSS modules. Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,38 @@ import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css'
 
 const App = () => {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'JavaScript 2', body: 'Description'},
-    {id: 3, title: 'JavaScript 3', body: 'Description'},
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
   ])
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Phyton', body: 'Description'},
-    {id: 2, title: 'Phyton 2', body: 'Description'},
-    {id: 3, title: 'Phyton 3', body: 'Description'},
-  ])
+  const [title, setTitle] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title);
+  }
 
   return (
     <div className="App">
+      <form action="">
+        {/* Управляемый компонент */}
+        <MyInput
+          onChange={e => setTitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="Название поста" 
+        />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList almira={posts} sagynbek={'Посты про JS'} />
-      <PostList almira={posts2} sagynbek={'Посты про Phyton'} />
     </div>
   )
 }
